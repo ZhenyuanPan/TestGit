bd8fdcb4a6a62a100137f37fc54d92b409637e14
zhenyuan Pan <1656922908@qq.com>
2020-07-06 10:21:52 +0800

    Hello world, my GitHub

---
+++ Git操作测试/Git操作测试/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Git操作测试
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
